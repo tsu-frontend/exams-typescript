26ea9bc7c37357c3a80f6b72c2dbf025be470da8
arzu <jalalovaarzu@gmail.com>
2025-08-05 12:37:19 +0400

    task-3

---
+++ arzu_jalalova/dadastureba_3/index.ts
@@ -0,0 +1,47 @@
+type Product = {
+  brand: string;
+  model: string;
+  price: number;
+};
+
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
+const product: Product = {
+  brand: "Apple",
+  model: "iPhone 14",
+  price: 999
+};
+
+const user: User = {
+  firstName: "Arzu",
+  lastName: "Jalalova",
+  email: "arzu@example.com",
+  phone: "+995599000000"
+};
+
+// DOM-ში გამოტანა
+const productDiv = document.getElementById("product-info");
+const userDiv = document.getElementById("user-info");
+
+if (productDiv) {
+  productDiv.innerHTML = `
+    <h2>Product Info</h2>
+    <p>Brand: ${product.brand}</p>
+    <p>Model: ${product.model}</p>
+    <p>Price: ${product.price}</p>
+  `;
+}
+
+if (userDiv) {
+  userDiv.innerHTML = `
+    <h2>User Info</h2>
+    <p>Name: ${user.firstName} ${user.lastName}</p>
+    <p>Email: ${user.email}</p>
+    <p>Phone: ${user.phone}</p>
+  `;
+}
